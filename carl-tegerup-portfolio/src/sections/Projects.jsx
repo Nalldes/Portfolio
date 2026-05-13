2219e4ab30f9000e92502bbbbc06446d74c322fc
Nalldes <153370075+Nalldes@users.noreply.github.com>
2026-05-13 13:46:05 +0200

    tags på projekt

--- carl-tegerup-portfolio/src/sections/Projects.jsx
+++ carl-tegerup-portfolio/src/sections/Projects.jsx
@@ -7,36 +7,136 @@ function Projects() {
       title: "Men in Black",
       description:
         "Ett projekt där jag och mina gruppmedlemmar arbetade mot lärarnas kravlista på systemet. Vi programmerade i Java och använde oss av Git för versionshantering samt en MySQL-databas.",
+      tags: [
+        {
+          name: "Java",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        },
+        {
+          name: "MySQL",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        },
+        {
+          name: "GitHub",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+      ],
     },
     {
       id: 2,
       title: "Hattmakarna",
       description:
         "Ett system för en hattmakare där fokuset låg på att arbeta inom Scrums ramverk. Kravlistan på systemet utökades i takt med projektets gång. Vi utvecklade med Java med JForms och även här implementerades en MySQL-databas samt Git för versionshantering.",
+      tags: [
+        {
+          name: "Java",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        },
+        {
+          name: "MySQL",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+        },
+        {
+          name: "GitHub",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+      ],
     },
     {
       id: 3,
       title: "Dammsjön Vassklippare",
       description:
         "Ett projekt med fokus på att lära oss grunderna i webbutveckling med html och css där vi skapade en hemsida för en fiktiv vassklipparåterförsäljare. Vi programmerade med hjälp av Java även här och använde Git för versionshantering.",
+      tags: [
+        {
+          name: "HTML",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        },
+        {
+          name: "CSS",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        },
+        {
+          name: "Java",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
+        },
+        {
+          name: "GitHub",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+      ],
     },
     {
       id: 4,
       title: "Podcast-hanterare",
       description:
         "Ett projekt där vi utvecklade en bibliotekapplikation som hanterar podcasts. Vi arbetade exclusivt i C# och använde Git för versionshantering.",
+      tags: [
+        {
+          name: "C#",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+        },
+        {
+          name: "GitHub",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+      ],
     },
     {
       id: 5,
       title: "CV-hanterare",
       description:
         "Utvecklade en komplett webbapplikation i C#, ASP.NET, och Entity Framework. Koden är strukturerad med en flerlagersarkitektur.",
+      tags: [
+        {
+          name: "C#",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-original.svg",
+        },
+        {
+          name: "ASP.NET",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dotnetcore/dotnetcore-original.svg",
+        },
+        {
+          name: "GitHub",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+      ],
     },
     {
       id: 6,
       title: "HealthStrive",
       description:
         "Mitt slutprojekt som jag gjorde i samband med min examen på Örebro Universitet. Arbetade tillsammans med två goda vänner och vi lyckades skapa en fungerande webbplattform för att spåra kost, ge tips om träning och dagliga hälsomål. Vi utvecklade frontend i JavaScript/React, HTML, CSS. Backend med hjälp av Node.js och implementerade en SQL-databas i molnet genom Azure.",
+      tags: [
+        {
+          name: "React",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        },
+        {
+          name: "JavaScript",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        },
+        {
+          name: "Node.js",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        },
+        {
+          name: "Azure",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg",
+        },
+        {
+          name: "GitHub",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        },
+        {
+          name: "HTML",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+        },
+        {
+          name: "CSS",
+          icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+        },
+      ],
     },
   ];
 
@@ -52,6 +152,15 @@ function Projects() {
           <div key={project.id} className="project-card">
             <h3>{project.title}</h3>
             <p>{project.description}</p>
+            <hr className="project-divider" />
+            <div className="project-tags">
+              {project.tags.map((tag) => (
+                <div key={tag.name} className="project-tag">
+                  <img src={tag.icon} alt={tag.name} />
+                  <span>{tag.name}</span>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
