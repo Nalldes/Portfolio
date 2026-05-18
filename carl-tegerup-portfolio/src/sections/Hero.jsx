cf29bcc570e5399a8f43b2bf45597443f8b82446
Nalldes <153370075+Nalldes@users.noreply.github.com>
2026-05-18 20:03:44 +0200

    Carl animation

--- carl-tegerup-portfolio/src/sections/Hero.jsx
+++ carl-tegerup-portfolio/src/sections/Hero.jsx
@@ -1,14 +1,39 @@
+import { useState, useEffect } from "react";
 import "../styles/Hero.css";
 import profileImg from "/calle cv bild.jpg";
 
 function Hero() {
+  const nameToType = "Carl.";
+  const [currentName, setCurrentName] = useState("");
+  const typingSpeed = 200;
+  const startDelay = 500;
+
+  useEffect(() => {
+    const startTimeout = setTimeout(() => {
+      let currentIndex = 0;
+
+      const typingInterval = setInterval(() => {
+        if (currentIndex < nameToType.length) {
+          setCurrentName(nameToType.substring(0, currentIndex + 1));
+          currentIndex++;
+        } else {
+          clearInterval(typingInterval);
+        }
+      }, typingSpeed);
+
+      return () => clearInterval(typingInterval);
+    }, startDelay);
+
+    return () => clearTimeout(startTimeout);
+  }, []);
+
   return (
     <section id="om-mig">
       <div className="hero-avatar">
         <img src={profileImg} alt="Carl Tegerup" />
       </div>
       <h1>
-        Hej, mitt namn är <span>Carl</span>
+        Hej, mitt namn är <span className="typed-name">{currentName}</span>
       </h1>
       <p>
         Jag är en nyexaminerad student som fått upp intresset för programmering
